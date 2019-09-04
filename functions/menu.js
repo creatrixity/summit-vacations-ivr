5d83b8e07be7cea283c2e114b903a78694d837fa
Caleb Mathew <creatrixity@gmail.com>
2019-09-04 20:47:44 +0100

    update log params

--- functions/menu.js
+++ functions/menu.js
@@ -5,7 +5,7 @@ const IvrRepository = require('../app/Repositories/IvrRepository')
 exports.handler = (event, context, callback) => {
   const { Digits, To } = event.queryStringParameters
 
-  console.log({ Digits, To })
+  console.log({ Digits, To, w: JSON.stringify(event.queryStringParameters), a: JSON.stringify(event.body) })
 
   const ivrRepo = new IvrRepository()
   const body = ivrRepo.navigateMainMenu(parseInt(Digits), { callerPhone: To })

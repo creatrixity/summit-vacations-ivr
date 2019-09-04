7ce73e42b9d93dd55a1f2cf61a5229c150f60f27
Caleb Mathew <creatrixity@gmail.com>
2019-09-04 23:58:03 +0100

    parse query string

--- functions/menu.js
+++ functions/menu.js
@@ -4,7 +4,7 @@ const IvrRepository = require('../app/Repositories/IvrRepository')
 const { pickQueryStringValue } = require('../app/Utilities/String')
 
 exports.handler = (event, context, callback) => {
-  const params = JSON.stringify(event.body)
+  const params = JSON.stringify(event.queryStringParameters)
   const Digits = pickQueryStringValue('Digits', params)
   const To = pickQueryStringValue('To', params)
 

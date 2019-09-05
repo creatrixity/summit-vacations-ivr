e6c9a2e4226f32f0c64ebf549e6c12d289a203bf
Caleb Mathew <creatrixity@gmail.com>
2019-09-05 10:55:04 +0100

    parse the event body in string form

--- functions/menu.js
+++ functions/menu.js
@@ -4,9 +4,11 @@ const IvrRepository = require('../app/Repositories/IvrRepository')
 const { pickQueryStringValue } = require('../app/Utilities/String')
 
 exports.handler = (event, context, callback) => {
-  const params = JSON.stringify(event.queryStringParameters)
+  const params = JSON.stringify(event.body)
   const Digits = pickQueryStringValue('Digits', params)
   const To = pickQueryStringValue('To', params)
+
+  console.log({ body: params, params: JSON.stringify(event.queryParameters) })
 
   const ivrRepo = new IvrRepository()
   const body = ivrRepo.navigateMainMenu(parseInt(Digits), { callerPhone: To })

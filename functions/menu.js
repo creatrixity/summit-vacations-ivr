114a3288cd96f53717797bbf1fbb9d0dc5e5fd8a
Caleb Mathew <creatrixity@gmail.com>
2019-09-05 11:44:12 +0100

    decodeURI invoked before parse

--- functions/menu.js
+++ functions/menu.js
@@ -4,17 +4,13 @@ const IvrRepository = require('../app/Repositories/IvrRepository')
 // const { pickQueryStringValue } = require('../app/Utilities/String')
 
 exports.handler = (event, context, callback) => {
-  const params = JSON.parse(decodeURI(event.body))
-  // const Digits = pickQueryStringValue('Digits', params)
-  // const To = pickQueryStringValue('To', params)
-
-  console.log({ body: params, params: JSON.stringify(event.queryParameters) })
+  const { Digits, To } = JSON.parse(decodeURI(event.body))
 
   const ivrRepo = new IvrRepository()
-  // const body = ivrRepo.navigateMainMenu(parseInt(Digits), { callerPhone: To })
+  const body = ivrRepo.navigateMainMenu(parseInt(Digits), { callerPhone: To })
 
   return callback(null, {
     statusCode: 200,
-    body: 'Hello'
+    body
   })
 }
